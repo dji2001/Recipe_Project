582ae76906953ed7456657781a2e7f8080df93aa
dji2001 <duojijiang2001@gmail.com>
2021-10-18 18:07:16 -0400

    integrated two way accessing into printall

--- src/printAll.java
+++ src/printAll.java
@@ -22,26 +22,47 @@ class printAll{
 			System.out.println(recipeName.substring(0, recipeName.indexOf(".")));
 		}
 	}
-    public static void printRecipe(String name) throws IOException {
-
+	public static void searchRecipe(String name) throws IOException {
+    	Scanner scanner = new Scanner(System.in);
+    	System.out.println();
 		File[] files = new File("./recipes").listFiles();
-		//If this pathname does not denote a directory, then listFiles() returns null. 
+		System.out.println(files);
+		if (files == null) {
+			System.out.println("test");
+		}
+
 		boolean fileFound = false;
 		for (File tempFile : files) {
 		    if (tempFile.isFile()) {
 		    	String temp = tempFile.getName();
-		       
-				if (temp.substring(0, temp.indexOf(".")).equals(name)) {
-						System.out.println("Opening the recipe for you:");
-						System.out.println(temp.substring(0, temp.indexOf(".")));
+		    	String temp2 = temp);
+				if (temp2.substring(0, temp2.indexOf(".")).equals(name)) {
+						System.out.println("If you would you like to read the entire recipe, enter '1'");
+						System.out.println("However, if you would like to read one instruction at a time, enter '2'");
+						String answer = scanner.nextLine();
+						while (!(answer.equals("1")) && !(answer.equals("2"))) {
+							System.out.println("Sorry, please enter '1' or '2'.");
+							answer = scanner.nextLine();
+						}
 						fileFound = true;
 						String fileName = "./recipes/" + temp.substring(0, temp.indexOf(".")) + ".txt";
 						File file = new File(fileName);
 						BufferedReader br = new BufferedReader(new FileReader(file));
 						String read;
-						while ((read = br.readLine()) != null) {
-				            System.out.println(read);
+						if (answer.equals("1")) {
+							while ((read = br.readLine()) != null) {
+					            System.out.println(read);
+							}
 						}
+						else {
+							while ((read = br.readLine()) != null) {
+								System.out.println("Type any character to read the next step or ingredient.");
+								String next = scanner.nextLine();
+					            System.out.println(read);
+							}
+						}
+						
+						
 						
 				}
 				
@@ -50,7 +71,7 @@ class printAll{
 		    
 		}  
 		if (!fileFound) {
-			System.out.println("Sorry, that is not a recipe in database");
+			System.out.println("Sorry, that is not an existing recipe.");
 		}
 		
     }
